3912a36a95a736a25d8335f79c9fdf188c7b21a5
AlexRzh <a.rzhevtsev@netrika.ru>
2018-12-01 00:56:28 +0300

    thirst

--- src/Trucks.java
+++ src/Trucks.java
@@ -1,2 +1,28 @@
-public class Trucks {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Trucks
+{
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Число ящиков: ");
+        int a = Integer.parseInt(reader.readLine());
+
+        int b = a/27;
+
+        int c = b/12;
+
+        for (int j = 1; j < c; j++) {
+            System.out.println("Грузовик " + j + ":");
+            for (int y = 1; y < 12; y++) {
+                System.out.println("Контейнер " + y + ":");
+                for (int i = 1; i < 27; i++) {
+                    System.out.println("Ящик " + i);
+                }
+            }
+        }
+
+    }
 }
